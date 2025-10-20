90cf36c929384ee8f86485d07eeefc1f2331517c
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-10-20 17:02:51 +0900

    feat: react버전업,react컴파일러,그외 기초 라이브러리 설치

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import reactHooks from "eslint-plugin-react-hooks";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    plugins: { "react-hooks": reactHooks },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": [
+        "error",
+        { additionalHooks: "(useEffectEvent|useOptimistic)" },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
