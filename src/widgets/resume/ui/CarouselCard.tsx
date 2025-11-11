1b117346c65d851a14146a339867db2627eacfc2
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-11-11 22:31:34 +0900

    fix: portfolio

--- src/widgets/resume/ui/CarouselCard.tsx
+++ src/widgets/resume/ui/CarouselCard.tsx
@@ -1,62 +1,81 @@
-'use client'
+'use client';
 
-import { CarouselItem } from "../consts/portfolio";
-import { Github } from "@/shared/icon";
-import { useFlag } from "@/shared/hooks/use-flag";
-import Link from "next/link";
-import { FlipFaceImage } from "./FlipFaceImage";
-import { cn } from "@/shared/lib/utils";
+import { CarouselItem } from '../consts/portfolio';
+import { Github } from '@/shared/icon';
+import { useFlag } from '@/shared/hooks/use-flag';
+import Link from 'next/link';
+import { cn } from '@/shared/lib/utils';
+import Image from 'next/image';
 
 export function CarouselCard({ item }: { item: CarouselItem }) {
-
   const [flag, toggle] = useFlag();
 
   return (
-    <article className="group [perspective:1000px] h-170 sm:h-120 md:h-120 lg:h-140 xl:h-120 shrink-0 basis-9/11 sm:basis-6/8 md:basis-5/8 lg:basis-2/5 m-4">
+    <article className="m-4 shrink-0 basis-9/11 sm:basis-6/8 md:basis-5/8 lg:basis-2/5">
       <div
         className={cn(
-          "relative w-full h-full text-foreground whitespace-pre-wrap break-keep text-pretty hyphens-auto",
-          "transition-transform duration-700 [transform-style:preserve-3d]",
-          "will-change-transform transform-gpu",
-          flag && "rotate-y-180"
+          'group text-foreground text-very-pretty relative h-full w-full rounded-lg',
+          'transition-transform duration-700 transform-3d',
+          'transform-gpu will-change-transform',
+          'shadow-[0_0_.6rem_0_rgb(68_68_68_/_82%)]',
+          flag && 'rotate-y-180'
         )}
+        onClick={toggle}
       >
-       {/* 앞면: flag=false일 때 보이도록 (회전 없이) */}
-        <div className="p-4 flex flex-col gap-2" onClick={toggle}>
-        <h1 className="rounded-lg bg-highlight text-white font-extrabold px-4 py-1 text-base mb-2 w-fit">{item.title}{item.subtitle && <span className="text-sm font-medium"> - {item.subtitle}</span>}</h1>
-        <p className="pb-1 border-b-2 w-full text-sm text-foreground/50">{item.date}</p>
-        <p className="font-black">{item.description}</p>
-        <ul className="list-disc pl-3 space-y-1 text-sm mb-2">
-          {item.contribution.map((c) => (
-          <li key={c}>{c}</li>
-        ))}
-        </ul>
-        <div className="flex flex-wrap gap-2">
-          {item.stack.map((tech) => (
-            <span
-            key={tech}
-            className="whitespace-nowrap rounded-full border border-border bg-muted text-foreground/80 px-2 py-0.5 text-xs"
+        <div className="absolute inset-0 overflow-hidden rounded-lg shadow-[0_0_.6rem_0_rgb(68_68_68_/_82%)] backface-hidden">
+          <Image
+            src={item.image}
+            alt="portfolio image"
+            fill
+            className="object-cover object-center"
+          />
+          <div
+            className={cn(
+              'pointer-events-none absolute inset-0 z-10 flex items-center justify-center',
+              'bg-black/70',
+              'opacity-0 transition-opacity duration-300',
+              'group-hover:opacity-100'
+            )}
           >
-            {tech}
-          </span>
-        ))}
-      </div>
-      <Link href={item.link} className="ml-auto">
-        <Github className="size-6" />
-      </Link>    
-      </div>
-
-        {/* 뒷면: flag=true일 때 보이도록 (면 자체를 180도) */}
-        <FlipFaceImage src={item.image} alt="Back" back onClick={toggle} />
+            <div className="pointer-events-none relative">
+              <div className="absolute inset-0 animate-ping rounded-full border-2 border-white/40" />
+              <div className="relative px-3 py-1 text-xl font-bold text-white">
+                View details
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex rotate-y-180 flex-col gap-2 p-4 backface-hidden">
+          <h1 className="bg-highlight mb-2 w-fit rounded-lg px-4 py-1 text-base font-extrabold text-white">
+            {item.title}
+            {item.subtitle && (
+              <span className="text-sm font-medium"> - {item.subtitle}</span>
+            )}
+          </h1>
+          <p className="text-foreground/50 w-full border-b-2 pb-1 text-sm">
+            {item.date}
+          </p>
+          <p className="font-black">{item.description}</p>
+          <ul className="mb-2 list-disc space-y-1 pl-3 text-sm">
+            {item.contribution.map((c) => (
+              <li key={c}>{c}</li>
+            ))}
+          </ul>
+          <div className="flex flex-wrap gap-2">
+            {item.stack.map((tech) => (
+              <span
+                key={tech}
+                className="border-border bg-muted text-foreground/80 rounded-full border px-2 py-0.5 text-xs whitespace-nowrap"
+              >
+                {tech}
+              </span>
+            ))}
+          </div>
+          <Link href={item.link} className="ml-auto">
+            <Github className="size-6" />
+          </Link>
+        </div>
       </div>
     </article>
-
-
-
-
-
-      // <div className="p-4 flex flex-col gap-2">
-      //   <h2>굿</h2>
-      // </div>
-  )
+  );
 }
