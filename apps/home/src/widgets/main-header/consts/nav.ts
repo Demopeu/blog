36f4d92c0df7e1c4255f46832e6afc7afe0bed5a
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-11-19 00:37:20 +0900

    feat: vercel.json

--- apps/home/src/widgets/main-header/consts/nav.ts
+++ apps/home/src/widgets/main-header/consts/nav.ts
@@ -1,11 +1,10 @@
 export type NavBarProps = {
   href: string;
   label: string;
-  isExternal?: boolean;
 };
 
 export const navItems: NavBarProps[] = [
   { href: '/', label: 'Home' },
   { href: '/resume', label: 'Resume' },
-  { href: process.env.NEXT_PUBLIC_BLOG_URL ?? '/', label: 'Blog', isExternal: true },
+  { href: '/blog', label: 'Blog' },
 ];
