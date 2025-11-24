f4540f9fa29459caeb17c445681f8012289666fc
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-11-24 20:53:49 +0900

    fix: nav.ts

--- apps/blog/src/widgets/main/consts/nav.ts
+++ apps/blog/src/widgets/main/consts/nav.ts
@@ -6,9 +6,15 @@ type CategoryProp = {
 
 export const categories: CategoryProp[] = [
   { name: '전체', href: '/' },
-  { name: '개발', href: '/dev', slug: 'dev' },
+  { name: 'TS', href: '/typescript', slug: 'typescript' },
   { name: '문제풀이', href: '/problem-solving', slug: 'problem-solving' },
+  { name: '개발', href: '/dev', slug: 'dev' },
   { name: '프로젝트', href: '/project', slug: 'project' },
 ];
 
-export const VALID_CATEGORIES = ['dev', 'problem-solving', 'project'];
+export const VALID_CATEGORIES = [
+  'dev',
+  'problem-solving',
+  'project',
+  'typescript',
+];
