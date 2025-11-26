6aee2dc7b3e174fd95692ddd1b11dae8217c57c5
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-11-26 11:38:14 +0900

    fix: microfrontends로 프록시 변경

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 import { withMicrofrontends } from '@vercel/microfrontends/next/config';
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   reactCompiler: true,
   transpilePackages: ['@repo/ui'],
   images: {
